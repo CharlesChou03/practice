878044ef8d66e18396b1e8028fc9d3b175587db2
Charles Chou <tycoon0049@gmail.com>
2021-12-01 17:33:24 +0800

    practice makefile

---
+++ make_file/2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int funcTwo(void) {
+    printf("Hello! World! 2.cpp 1\n");
+    printf("哈囉！C 語言！\n");
+
+    return 0;
+}
---
+++ make_file/3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int funcThree(void) {
+    printf("Hello! World!3.cpp\n");
+    printf("哈囉！C 語言！\n");
+
+    return 0;
+}
